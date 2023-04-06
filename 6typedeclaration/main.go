c26fcef0c6251eb5cab8d6b69382f4841834a650
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-04-06 09:20:25 +0700

    Ops Math

---
+++ 6typedeclaration/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	type nama string
+
+	var name nama = "NauQif"
+
+	fmt.Println(name)
+}
---
+++ 7opsmatematika/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 2
+	b := 3
+	c := 1
+
+	c += 3
+
+	fmt.Println(a + b - c * b)
+}
