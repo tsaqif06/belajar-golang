9bf40776aa100694cc4ecf0be841d7f5f4ce8580
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-04-06 08:49:20 +0700

    Materi tipe data dan variabel

---
+++ 2tipedata/bool.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Benar")
+	fmt.Println("Benar Sekali")
+	fmt.Println("Benar Sekali Anda")
+	
+	fmt.Println(len("Benar"))
+	fmt.Println("Benar Sekali"[0])
+	fmt.Println("Benar Sekali Anda"[3])
+}
---
+++ 3variabel/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Tsaqif"
+	fmt.Println(name)
+
+	name = "Ovang"
+	fmt.Println(name)
+
+	var name2 = 12
+	fmt.Println(name2)
+
+	name3 := "NauQif"
+	fmt.Println(name3)
+
+	var (
+		test1 = "Tsaqif"
+		test2 = "Naufal"
+	)
+	fmt.Println(test1, test2)
+}
